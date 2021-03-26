e1671f2511f88ee833b8df6f827a88ee624d6ec2
batmanba <berke.batman@cass.city.ac.uk>
2021-03-26 18:01:09 +0000

    progress

--- src/app.js
+++ src/app.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+const artistControllers = require('./controllers/artists')
 
 const { Artist } = require('../src/models');
 
-/// this file is the one that holds the https requests suchs as get, post etc.
-app.post('/artists', (req, res) => {
-   res.status(201).json({ result: 'Hello World' });
- });
+app.post('/artists', artistControllers.create);
 
 module.exports = app;
 
--- tests/artists.test.js
+++ tests/artists.test.js
@@ -1,48 +1,44 @@
-const { expect } = require('chai'); // chai is an assertion library, gives alternatives for testing, here we are using the expect function from chai  
+const { expect } = require('chai'); 
 
-const request = require('supertest'); //supertest eliminates the need to test through post, is a JS
-                                       // is a js tool used for end2end testing
+const request = require('supertest'); 
 
-const { Artist } = require('../src/models/index.js'); //this is the artist prototype (?)
+const { Artist } = require('../src/models'); 
 
-const app = require('../src/app');  //this requires the app, which hold all the api requests
+const app = require('../src/app');  
 
 const { response } = require('../src/app');
 
-//mocha/chai/supertest syntax is similar to jest. Shrort tutorial on the three tools: 
-//https://dev-tester.com/dead-simple-api-tests-with-supertest-mocha-and-chai/
-
 describe('/artists', () => {
-   
-   // this will sync the artist database, create it before every test
    before(done => {
-      Artist.sequilze
+      Artist.sequelize
       .sync()
          .then(() => done())
          .catch(error => done(error)); 
-   })
-
-   // this is to clear the database after running each test as test leftovers may affect latter tests in a negative way
+   });
    beforeEach(done => {
       Artist.destroy( { where: {} })
       .then(() => done().catch(error => done(error)));
-   })
+   });
 
-   // this test uses chai's expect function, we are making a post request to /artists API endpoint with an artist name and genre
-   // then we get a response which we expect to have a status of 201.
-   // we then use the catch catch an error
-   // details: here we we expect the /artists endpoint to have a functionailty through another file so that we can write a new artist to the 
    describe('Post /artists', () => {
       it('creates a new artist in the database', (done) => {
          request(app).post('/artists').send({
-            name: 'FKJ',
-            genre: 'Skyline'
+            name: 'BBKing',
+            genre: 'Blues'
          }).then(response => {
             expect(response.status).to.equal(201);
+            expect(response.body.name).to.equal('BBKing');
+            expect(response.body.genre).to.equal('Blues');
+
+            Artist.findByPk(response.body.id, {raw: true}).then(insertedArtistRecord => {
+               expect(insertedArtistRecord.name).to.equal('BBKing');
+               expect(insertedArtistRecord.genre).to.equal('Blues');
             done();
          }).catch(error => done(error));
-     })
-   })
+     });
+   });
 });
+});
+
 
 
